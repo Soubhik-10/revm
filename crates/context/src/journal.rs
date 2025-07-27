70a9b2645e461b4b6bcac5095efdeb6a38373525
Ishika Choudhury <117741714+Rimeeeeee@users.noreply.github.com>
2025-07-27 20:32:38 +0530

    better handling for featured gated glam

--- crates/context/src/journal.rs
+++ crates/context/src/journal.rs
@@ -103,12 +103,23 @@ impl<DB: Database, ENTRY: JournalEntryTr> JournalTr for Journal<DB, ENTRY> {
         &mut self.database
     }
 
+    #[cfg(not(feature = "glamsterdam"))]
     fn sload(
         &mut self,
         address: Address,
         key: StorageKey,
     ) -> Result<StateLoad<StorageValue>, <Self::Database as Database>::Error> {
-        self.inner.sload(&mut self.database, address, key, false)
+        self.inner.sload(&mut self.database, address, key)
+    }
+
+    #[cfg(feature = "glamsterdam")]
+    fn sload(
+        &mut self,
+        address: Address,
+        key: StorageKey,
+    ) -> Result<StateLoad<StorageValue>, <Self::Database as Database>::Error> {
+        self.inner
+            .sload_with_account(&mut self.database, address, key, false)
     }
 
     fn sstore(
