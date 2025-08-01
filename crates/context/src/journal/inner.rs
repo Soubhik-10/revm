7f1e861961d410888fb566d06cff491c49acab42
Ishika Choudhury <117741714+Rimeeeeee@users.noreply.github.com>
2025-08-01 17:29:55 +0530

    feat: perf check (#9)
    * feat: perf check
    
    * fixes
    
    * Revert "fixes"
    
    This reverts commit c5a930139aa928ae267fe3876882d03d0516af85.
    
    * fixes
    
    ---------
    
    Co-authored-by: Soubhik Singha Mahapatra <160333583+Soubhik-10@users.noreply.github.com>

--- crates/context/src/journal/inner.rs
+++ crates/context/src/journal/inner.rs
@@ -1040,7 +1040,12 @@ impl<ENTRY: JournalEntryTr> JournalInner<ENTRY> {
 
                 let acc = self.state.get_mut(&address).unwrap();
 
-                set_storage_access_reads(acc, self.transaction_id, key);
+                let tx_index = self.transaction_id as u64;
+                acc.storage_access
+                    .reads
+                    .entry(tx_index)
+                    .or_default()
+                    .insert(key);
 
                 return Ok(StateLoad::new(
                     SStoreResult {
@@ -1066,7 +1071,12 @@ impl<ENTRY: JournalEntryTr> JournalInner<ENTRY> {
         // insert value into present state.
         slot.present_value = new;
 
-        set_storage_change_write(acc, self.transaction_id, key, present_value, new);
+        let tx_index = self.transaction_id as u64;
+        acc.storage_access
+            .writes
+            .entry(tx_index)
+            .or_default()
+            .insert(key, (present_value, new));
 
         Ok(StateLoad::new(
             SStoreResult {
@@ -1219,7 +1229,13 @@ pub fn sload_with_account<DB: Database, ENTRY: JournalEntryTr>(
 
     // Set `StorageChange` for reads here
     if !from_sstore {
-        set_storage_access_reads(account, transaction_id, key);
+        let tx_index = transaction_id as u64;
+        account
+            .storage_access
+            .reads
+            .entry(tx_index)
+            .or_default()
+            .insert(key);
     }
 
     Ok(StateLoad::new(value, is_cold))
@@ -1276,34 +1292,4 @@ fn reset_preloaded_addresses(
         return;
     }
     warm_preloaded_addresses.clone_from(precompiles);
-}
-
-/// Sets storage change write for the account.(FOR GLAMSTERDAM)
-#[cfg(feature = "glamsterdam")]
-fn set_storage_change_write(
-    account: &mut Account,
-    transaction_id: usize,
-    key: StorageKey,
-    pre_value: StorageValue,
-    post_value: StorageValue,
-) {
-    let tx_index = transaction_id as u64;
-    account
-        .storage_access
-        .writes
-        .entry(tx_index)
-        .or_default()
-        .insert(key, (pre_value, post_value));
-}
-
-/// Sets storage access reads for the account.(FOR GLAMSTERDAM)
-#[cfg(feature = "glamsterdam")]
-fn set_storage_access_reads(account: &mut Account, transaction_id: usize, key: StorageKey) {
-    let tx_index = transaction_id as u64;
-    account
-        .storage_access
-        .reads
-        .entry(tx_index)
-        .or_default()
-        .insert(key);
 }
