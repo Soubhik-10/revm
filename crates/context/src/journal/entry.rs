33162230a68a6988b5f9fbeb3d328baaf05b2119
Soubhik-10 <soubhiksmp2004@gmail.com>
2025-08-04 17:52:32 +0530

    revert

--- crates/context/src/journal/entry.rs
+++ crates/context/src/journal/entry.rs
@@ -507,10 +507,14 @@ impl JournalEntryTr for JournalEntry {
 
             JournalEntry::NonceChange { address } => {
                 let account = state.get_mut(&address).unwrap();
+                let prev_nonce = account.info.nonce;
                 account.info.nonce -= 1;
                 let transaction_id = account.transaction_id as u64;
-                account.nonce_change.change.remove(&transaction_id);
+                if let Some(nonce_entry) = account.nonce_change.change.get_mut(&transaction_id) {
+                    *nonce_entry = (prev_nonce, account.info.nonce);
+                }
             }
+
             JournalEntry::AccountCreated {
                 address,
                 is_created_globally,
