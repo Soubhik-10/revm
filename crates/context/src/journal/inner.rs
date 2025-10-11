7ef70d7391feee4c5e6688f21ad4a29dd5ed638b
Soubhik-10 <soubhiksmp2004@gmail.com>
2025-10-11 19:01:46 +0530

    revert last

--- crates/context/src/journal/inner.rs
+++ crates/context/src/journal/inner.rs
@@ -850,11 +850,6 @@ impl<ENTRY: JournalEntryTr> JournalInner<ENTRY> {
                     db.code_by_hash(info.code_hash)?
                 };
                 info.code = Some(code);
-                if self.spec.is_enabled_in(SpecId::AMSTERDAM) {
-                    if let Some(ref code_ref) = info.code {
-                        load.data.code_change = code_ref.bytecode().clone();
-                    }
-                }
             }
         }
 
